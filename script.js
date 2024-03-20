fafd67c55a709b6bf4f759b7d7e42483f4966a55
PortelaZod <jeffersonportela@live.com>
2024-03-20 13:45:24 -0300

    2.1
    mudanças no estilo da barra de links oculta e adição de galeria Modal.

---
+++ script.js
@@ -0,0 +1,12 @@
+const modalFrame=document.querySelector(".modal-frame")
+const imgModal=document.querySelector(".modal-frame img")
+
+
+function abrirModal(src){
+    modalFrame.style.visibility="visible"
+    imgModal.src=(src)
+}
+
+function fecharModal(){
+    m.style.visibility="hidden"
+}
