3348142d3fc2a9498fba627640802d8a54b2d851
PortelaZod <jeffersonportela@live.com>
2024-03-22 02:41:39 -0300

    Novas Imagens e Correção de alguns problemas.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const modalFrame=document.querySelector(".modal-frame")
 const imgModal=document.querySelector(".modal-frame img")
+const whatsBtn=document.querySelector(".whats-btn")
 function abrirModal(src){
     modalFrame.style.visibility="visible"
     imgModal.src=(src)
+    whatsBtn.style.display="none"
 }
 
 function fecharModal(){
-    m.style.visibility="hidden"
+    modalFrame.style.visibility="hidden"
 }
 // Modal Efect
