fda0e84ba45483af640e16c03d3d824a0fa23433
PortelaZod <jeffersonportela@live.com>
2024-03-22 03:49:34 -0300

    2.5
    Layout 2.5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const modalFrame=document.querySelector(".modal-frame")
 const imgModal=document.querySelector(".modal-frame img")
 const whatsBtn=document.querySelector(".whats-btn")
+const loader=document.querySelector(".container-loader").style
 function abrirModal(src){
     modalFrame.style.visibility="visible"
     imgModal.src=(src)
@@ -11,3 +12,8 @@ function fecharModal(){
     modalFrame.style.visibility="hidden"
 }
 // Modal Efect
+
+
+function load(){
+    loader.display="none"
+}
