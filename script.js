ab858eed03d694b8fe98f0313eb211962d285d38
PortelaZod <jeffersonportela@live.com>
2024-04-01 02:29:40 -0300

    2.7
    animação no botão whats, modal frame no video galeria.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const modalFrame=document.querySelector(".modal-frame")
 const imgModal=document.querySelector(".modal-frame img")
+const Mfv=document.querySelector(".modal-frameVideo")
+const Mv=document.querySelector(".modal-frameVideo video")
 const whatsBtn=document.querySelector(".whats-btn")
 const loader=document.querySelector(".container-loader").style
 
@@ -17,6 +19,19 @@ function fecharModal(){
 }
 // Modal Efect
 
+
+function abrirVideo(x){
+    Mfv.style.visibility="visible"
+    Mfv.style.transform="scale(1)"
+    whatsBtn.style.display="none"
+    Mv.src=(x)
+}
+
+function fecharVideo(){
+    Mfv.style.visibility="hidden"
+    Mfv.style.transform="scale(0)"
+    whatsBtn.style.display="flex"
+}
 
 function load(){
     loader.display="none";
